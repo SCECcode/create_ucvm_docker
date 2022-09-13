edde4f4719356ae90abcf2897c2a3538295fa70e
Mei-Hui Su <mei@Mei-Huis-MacBook-Pro.local>
2022-09-13 10:43:42 -0700

    update the run_one scripts

--- ucvm_v22_7/run_one.sh
+++ ucvm_v22_7/run_one.sh
@@ -1,1 +1,3 @@
-docker run --rm -it -e APP_UNAME=`id -u -nr` -e APP_GRPNAME=`id -g -nr` -e APP_UID=`id -u` -e APP_GID=`id -g` --mount type=bind,source="$(pwd)"/target,destination=/app/target ucvm_227_cvmh:0710
+
+DATE=`date '+%m%d'`
+docker run --rm -it -e APP_UNAME=username -e APP_GRPNAME=`id -g -nr` -e APP_UID=`id -u` -e APP_GID=`id -g` --mount type=bind,source="$(pwd)"/target,destination=/app/target ucvm_227_cvmh:$DATE
