e830f86796f2863f9b04b3531aadd196f8847577
Mei-Hui Su <mei@MeiPro.local>
2022-09-20 09:33:38 -0700

    tweak permission

--- scripts/start_cvms5_mac.sh
+++ scripts/start_cvms5_mac.sh
@@ -1,3 +1,4 @@
+## for mac with X service(XQuartz)
 # Set Mac IP address
 IP=`ifconfig -l | xargs -n1 ipconfig getifaddr`
 
@@ -5,7 +6,12 @@ echo "Remember to run : xhost - "$IP
 # Allow connections from Mac to XQuartz
 /opt/X11/bin/xhost + "$IP"
 
-docker run --platform linux/amd64 --rm -it -e DISPLAY=$"${IP}:0"  --mount type=bind,source="$(pwd)"/target,destination=/app/target sceccode/ucvm_227_cvms5:latest
+## special case check for M1
+if [[ $(uname -m) == 'arm64' ]]; then
+  docker run --platform=linux/amd64 --rm -it --mount type=bind,source="$(pwd)"/target,destination=/app/target sceccode/ucvm_227_cvms5:latest
+else
+  docker run --rm -it --mount type=bind,source="$(pwd)"/target,destination=/app/target sceccode/ucvm_227_cvms5:latest
+fi
 
 /opt/X11/bin/xhost - "$IP"
 
