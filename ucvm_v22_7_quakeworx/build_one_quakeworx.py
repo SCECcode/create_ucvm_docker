2fab12cf56d4d49dd2a23fbc737f048ea011a0e6
meihuisu <mei@usc.edu>
2025-01-18 23:59:24 -0800

    local

--- ucvm_v22_7_quakeworx/build_one_quakeworx.py
+++ ucvm_v22_7_quakeworx/build_one_quakeworx.py
@@ -24,16 +24,16 @@ if __name__ == '__main__':
 
     for m in models:
         print ("building model: " + m)
-        cmd = "docker build --no-cache=false -f Dockerfile . -t ucvm_227_%s_quakeworx:%s " \
-            "--build-arg APP_UNAME=ucvmuser --build-arg APP_GRPNAME=ucvmuser " \
+        cmd = "docker build --no-cache -f Dockerfile . -t tucvm_227_%s_quakeworx:%s " \
+            "--build-arg APP_UNAME=qwxdev --build-arg APP_GRPNAME=qwxdev " \
             "--build-arg APP_UID=1000 --build-arg APP_GID=1000 --build-arg MODELID=%s --build-arg BDATE=%s"%(m,mdate,m,mdate)
         os.system(cmd)
 
-        cmd = "docker tag ucvm_227_%s_quakeworx:%s sceccode/ucvm_227_%s_quakeworx:%s"%(m,mdate,m,mdate)
+        cmd = "docker tag tucvm_227_%s_quakeworx:%s sceccode/tucvm_227_%s_quakeworx:%s"%(m,mdate,m,mdate)
         print(cmd)
-        os.system(cmd)
+#        os.system(cmd)
 
-        print("pushing models: " + m + " with sceccode/ucvm_227_%s_quakeworx:%s" % (m,mdate))
-        cmd = "docker push sceccode/ucvm_227_%s_quakeworx:%s" % (m,mdate)
+        print("pushing models: " + m + " with sceccode/tucvm_227_%s_quakeworx:%s" % (m,mdate))
+        cmd = "docker push sceccode/tucvm_227_%s_quakeworx:%s" % (m,mdate)
         print(cmd)
-        os.system(cmd)
+#        os.system(cmd)
