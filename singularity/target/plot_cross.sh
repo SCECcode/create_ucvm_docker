ac853cc1d96d6e48a7e59d66f11804db680438a9
meihuisu <mei@usc.edu>
2025-01-24 11:03:53 -0800

    add singularity deffile

---
+++ singularity/target/plot_cross.sh
@@ -0,0 +1,2 @@
+plot_cross_section.py -s 0 -h 1176 -d vs -c cvmh -a sd -o ../result/cross_cvmh.png -i $UCVM_INSTALL_PATH -v 50 -b 33.9542,-118.7512 -u 33.9651,-117.5753 -e 5000
+
---
+++ singularity/target/query.sh
@@ -0,0 +1,2 @@
+ucvm_query -m cvmh -f $UCVM_INSTALL_PATH/conf/ucvm.conf -l 34.11,-118.08,1000
+
