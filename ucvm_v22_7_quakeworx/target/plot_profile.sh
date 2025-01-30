6f3071533b42de9a9e03ea01b47a8a4caa788a46
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2025-01-30 07:39:45 +0000

    adjust directory layers/permissions

--- ucvm_v22_7_quakeworx/target/plot_profile.sh
+++ ucvm_v22_7_quakeworx/target/plot_profile.sh
@@ -1,3 +1,3 @@
 
-plot_depth_profile.py -i $UCVM_INSTALL_PATH -d vs -c cvmh -o ../../result/profile_cvmh.png -C 'CVM-H v15.1.1' -v 100 -b 0 -s 34.0625,-118.1633 -e 30000
+plot_depth_profile.py -i $UCVM_INSTALL_PATH -d vs -c cvmh -o ../result/profile_cvmh.png -C 'CVM-H v15.1.1' -v 100 -b 0 -s 34.0625,-118.1633 -e 30000
 
